686a2c58dfc3822592dec50aca1427ea0868befe
Jacob Cheney <jacobx10@gmail.com>
2023-03-14 15:46:45 -0700

    Added default window for all builds, specifically allows web builds to have default window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+
 fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // For web builds for embedding into <div>'s with id=bevy
+                canvas: Some("#bevy".to_string()),
+                fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
     println!("Hello, world!");
 }
