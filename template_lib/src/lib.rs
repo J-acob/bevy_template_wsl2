c0d3ea3152e02bd765b362e2e0ed5dad4cce1efb
Jacob Cheney <jacobx10@gmail.com>
2023-09-11 13:32:48 -0700

    Update to have better defaults

--- template_lib/src/lib.rs
+++ template_lib/src/lib.rs
@@ -1,8 +1,20 @@
-pub fn template_hello(person: &str) {
-    println!("Hello, {:?}", person);
-}
+use bevy::{app::PluginGroupBuilder, prelude::*};
+use hello::HelloPlugin;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod hello;
+
+pub mod prelude {
+    pub use bevy::prelude::*;
+}
+pub struct TemplatePlugins;
+
+impl PluginGroup for TemplatePlugins {
+    fn build(self) -> PluginGroupBuilder {
+        let mut group = PluginGroupBuilder::start::<Self>();
+
+        // Add any of the library plugins here, conditionally if need be.
+
+        group = group.add(HelloPlugin);
+        group
+    }
 }
